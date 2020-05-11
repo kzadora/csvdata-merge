9f3703dcf3f690584beb4ae4eb76176640e0e68e
Karol Zadora-Przylecki <kzadora@live.com>
2020-05-10 18:10:01 -0700

    dmerge method WIP

--- lib/dmerge.py
+++ lib/dmerge.py
@@ -22,23 +22,64 @@ def dmerge(primary, auxiliaries, aux_props=None):
     ------
     ValueError
         If there are no auxiliary records
-        If some auxiliary records do not have attributes specified in aux_props
-        If auxiliary records do not have the same attributes (aux_props not provided)
+        If some auxiliary records do not have properties specified in aux_props
+        If differnt auxiliary records contribute different propertiess (aux_props not provided)
+        If any auxiliary records do not contribute any additional properties beyond these already present in the primary record (aux_props not provided)
 
     Notes
     -----
-    Each attribute from the auxiliary records is classified as "numeric" or "non-numeric".
-    Numeric attribute values are interpolated according to the time difference between the primary record precision timestamp
+    Each property from the auxiliary records is classified as "numeric" or "non-numeric".
+    Numeric property values are interpolated according to the time difference between the primary record precision timestamp
     and auxiliary record precision timestamp(s).
-    For non-numeric attributes the record closest to the primary record in terms of precision timestamp is selected as the attribute source.
+    For non-numeric properties the record closest to the primary record in terms of precision timestamp is selected as the property source.
     """
+
+    aux_props = validate_dmerge_args(primary, auxiliaries, aux_props)
+
+    if len(auxiliaries) == 1:
+        # TODO Simpler case where numeric properties do not have to be interpolated and the choice of "closest in time" record is obvious
+    else:
+        # TODO Complex case where numeric properties have to be interpolated and we need to compute which record is closest in time to the primary
+
+
+def validate_dmerge_args(primary, auxiliaries, aux_props=None):
+    """Utility method for validating arguments passed to dmerge() method
+
+    Returns
+    -------
+    list[str]
+        List of properties to merge into the primary record
+
+    See Also
+    --------
+    dmerge: for information about validation errors raised by this method
+    """"
 
     if len(auxiliaries) == 0:
         raise ValueError("Must pass at least one auxiliary record")
 
     if aux_props:
-        # TODO: verify that all auxiliary records have all attributes specified by aux_props
-        pass
+        for aux in auxiliaries:
+            if not aux.keys().issuperset(aux_props):
+                e = ValueError("Auxiliary record does not have all expected properties")
+                e.expectedProps = aux_props
+                e.record = aux
+                raise e
     else:
-        # TODO: verify that all auxiliary records have the same set of attributes
-        pass
+        aux_props = auxiliaries[0].keys() - primary.keys()        
+        
+        for aux in auxiliaries:
+            extra_props = aux.keys() - primary.keys()
+
+            if len(extra_props) == 0:
+                e = ValueError("Auxiliary record does not contribute any properties beyond these already in the primary record")
+                e.record = auxiliaries[0]
+                raise e
+
+            if aux_props != extra_props:
+                e = ValueError("Different auxiliary records contribute different properties")
+                e.record1 = auxiliaries[0]
+                e.record2 = aux
+                raise e
+
+    return aux_props
